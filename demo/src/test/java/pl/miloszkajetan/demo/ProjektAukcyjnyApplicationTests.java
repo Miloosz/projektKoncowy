d0a06258e73b132b2193c71432e181309a0a46bd
Miloosz <67183832+Miloosz@users.noreply.github.com>
2021-01-23 12:46:53 +0100

    Add files via upload
    świeżo wygenerowany spring boot.

---
+++ demo/src/test/java/pl/miloszkajetan/demo/ProjektAukcyjnyApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.miloszkajetan.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjektAukcyjnyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
